b3eb9601751366339bba0240c346523d0f6809de
Andrey A. Belokur <abelokur@yandex.ru>
2026-06-06 23:23:33 +0300

    fix: added MissingServletRequestParameterException to Exception handler.

--- core/interaction-api/src/main/java/ru/practicum/exception/BaseExceptionHandler.java
+++ core/interaction-api/src/main/java/ru/practicum/exception/BaseExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -36,6 +37,13 @@ public abstract class BaseExceptionHandler {
             ServiceTemporaryUnavailableException e) {
         log.warn("503 {}", e.getMessage(), e);
         return new ApiError("SERVICE_UNAVAILABLE", "Сервис временно недоступен", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+        log.warn("400 {}", e.getMessage(), e);
+        return new ApiError("BAD_REQUEST", "Ожидался обязательный параметр", e.getMessage());
     }
 
     @ExceptionHandler
